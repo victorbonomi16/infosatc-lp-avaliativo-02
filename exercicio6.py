673204c4785f48723be7c1eddf4f1bd0412ff2e8
victorbonomi16 <bononikis@gmail.com>
2020-09-06 19:43:06 -0300

    feat: Exercicio 6 concluido

---
+++ exercicio6.py
@@ -0,0 +1,13 @@
+livro = ["A culpa é das estrelas", " Diario de um banana"]
+print("Possuem na lista de livros os seguintes: ",livro)
+esportes = ["futebol", "voleibol","basquete"]
+print("Possuem na lista de esportes os seguintes: ",esportes)
+filme = ["DeVoltaProFuturo", "VingadoresUltimato", "Power" ]
+print("Possuem na lista de filmes os seguintes:  "  , filme)
+jogo = ["Fortnite","csgo","Fallguys","COD" ]
+print("Possuem na lista de jogos os seguintes: ", jogo)
+
+todaslistas = esportes + filme + jogo +livro
+print("A lista de todos os itens juntos é composta por: ",todaslistas)
+
+print("A minha lista possui um livro que é: ",livro[1])
