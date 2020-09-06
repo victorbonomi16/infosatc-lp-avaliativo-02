d011265648f85750b35b76692c81087e5d5aafaf
victorbonomi16 <bononikis@gmail.com>
2020-09-06 19:53:59 -0300

    feat: Exercicio 7 concluido

---
+++ exercicio7.py
@@ -0,0 +1,6 @@
+carrinho = ["GeForce RTX 2080Ti", "Gabinete Cooler Master", "placa mãe ASUS ROG Strix B460-G", "i9 9900k"," Memoria HyperX 8gb DDR4","Fonte EVGA 650W"," HD 1TB" ]
+print("seu carinho de compras tem: ", carrinho) 
+carrinho.remove("GeForce RTX 2080Ti")
+print("um item foi removido do seu carrinho, agora ele está assim: ",carrinho)
+carrinho.remove("Gabinete Cooler Master" )
+print("um item foi removido do seu carrinho, agora ele está assim: ",carrinho)
